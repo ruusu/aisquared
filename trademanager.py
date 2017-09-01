303ed4449cd120bb27ad4ac6333bccaeea83cfb6
Teemu Alin <teemu.alin@aalto.fi>
2017-09-01 12:05:57 +0300

    small fix to trademanager

--- trademanager.py
+++ trademanager.py
@@ -6,19 +6,25 @@ class portfolio:
 btcwallet = portfolio(1.0, 4000.0)
 
 def buybtc(price):
-	amount = btcwallet.eurbalance / price
-	btcwallet.cryptobalance += amount
-	print "Bought", amount, "BTC for", btcwallet.eurbalance, "EUR."
-	btcwallet.eurbalance = 0
-	print "Current portfolio status:"
-	print "BTC:", btcwallet.cryptobalance
-	print "EUR:", btcwallet.eurbalance
+	if btcwallet.eurbalance > 0:
+		amount = btcwallet.eurbalance / price
+		btcwallet.cryptobalance += amount
+		print "Bought", amount, "BTC for", btcwallet.eurbalance, "EUR."
+		btcwallet.eurbalance = 0
+		print "Current portfolio status:"
+		print "BTC:", btcwallet.cryptobalance
+		print "EUR:", btcwallet.eurbalance
+	else:
+		print "Not enough money to make a buy."
 
 def sellbtc(price):
-	amount = btcwallet.cryptobalance * price
-	btcwallet.eurbalance += amount
-	print "Sold", btcwallet.cryptobalance, "BTC for", amount, "EUR."
-	btcwallet.cryptobalance = 0
-	print "Current portfolio status:"
-	print "BTC:", btcwallet.cryptobalance
-	print "EUR:", btcwallet.eurbalance
+	if btcwaller.cryptobalance > 0:
+		amount = btcwallet.cryptobalance * price
+		btcwallet.eurbalance += amount
+		print "Sold", btcwallet.cryptobalance, "BTC for", amount, "EUR."
+		btcwallet.cryptobalance = 0
+		print "Current portfolio status:"
+		print "BTC:", btcwallet.cryptobalance
+		print "EUR:", btcwallet.eurbalance
+	else:
+		print "No BTC to sell."
