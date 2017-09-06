bb7b098d2d402549a58c5ee2e8d8bbea8a62603e
Teemu Alin <teemu.alin@aalto.fi>
2017-09-06 10:59:51 +0300

    trades now written to a file, main script can begin where it left off in case of rebooting.

--- aisquared.py
+++ aisquared.py
@@ -2,6 +2,7 @@ import time
 import json
 import requests
 from trademanager import buybtc, sellbtc
+import os
 
 def getPrice():
 	data=requests.get('https://api.coinbase.com/v2/prices/spot?currency=EUR')
@@ -11,20 +12,36 @@ def getPrice():
 	return price
 
 def main():
+	if not os.path.isfile("price.txt"):
+		file = open("price.txt", "w")
+		file.close()
+	if not os.path.isfile("trades.txt"):
+		file = open("trades.txt", "w")
+		file.close()
 	startingPrice = getPrice()
+	if os.path.getsize("price.txt") == 0:
+		file = open("price.txt", "w")
+		priceStr = str(startingPrice)
+		file.write("%s" % priceStr)
+		print "First run detected, price.txt initialized."
+	else:
+		file = open("price.txt", "r")
+		priceStr = file.readline()
+		startingPrice = float(priceStr)
 	print "Starting price is:", startingPrice
 	TRIGGER = 0.005
 	loop = 1
 	priceChange = 0
 	kierros = 0
 	while loop == 1:
-		print "Price change since last trade:", priceChange
+		if kierros == 1:
+			print "Price change since last trade:", priceChange
 		time.sleep(30)
 		newPrice = getPrice()
 		print "Current BTC price is:", newPrice
 		if kierros == 0:
 			priceChange = priceChange + newPrice - startingPrice
-			kierroes = 1
+			kierros = 1
 		else:
 			priceChange = priceChange + newPrice - newPrice2
 		time.sleep(30)
@@ -43,6 +60,10 @@ def main():
 					sellbtc(newPrice2)
 				priceChange = 0
 				startingPrice = newPrice2
+				priceStr = str(startingPrice)
+				file = open("price.txt", "w")
+				file.write("%s" % priceStr)
+				file.close()
 			else:
 				priceChange = newPrice2 - startingPrice
 
--- trademanager.py
+++ trademanager.py
@@ -1,3 +1,5 @@
+import datetime
+
 class portfolio:
 	def __init__(self, cryptobalance, eurbalance):
 		self.cryptobalance = cryptobalance
@@ -10,10 +12,16 @@ def buybtc(price):
 		amount = btcwallet.eurbalance / price
 		btcwallet.cryptobalance += amount
 		print "Bought", amount, "BTC for", btcwallet.eurbalance, "EUR."
+		amountstr = str(amount)
+		euramountstr = str(btcwallet.eurbalance)
 		btcwallet.eurbalance = 0
 		print "Current portfolio status:"
 		print "BTC:", btcwallet.cryptobalance
 		print "EUR:", btcwallet.eurbalance
+		file = open("trades.txt", "a")
+		file.write(datetime.datetime.now().ctime())
+		file.write(" BUY %s btc for %s eur.\n" % (amountstr, euramountstr))
+		file.close() 
 	else:
 		print "Not enough money to make a buy."
 
@@ -22,9 +30,15 @@ def sellbtc(price):
 		amount = btcwallet.cryptobalance * price
 		btcwallet.eurbalance += amount
 		print "Sold", btcwallet.cryptobalance, "BTC for", amount, "EUR."
+		amountstr = str(amount)
+		btcamountstr = str(btcwallet.cryptobalance)
 		btcwallet.cryptobalance = 0
 		print "Current portfolio status:"
 		print "BTC:", btcwallet.cryptobalance
 		print "EUR:", btcwallet.eurbalance
+		file = open("trades.txt", "a")
+		file.write(datetime.datetime.now().ctime())
+		file.write(" SELL %s btc for %s eur.\n" % (btcamountstr, amountstr))
+		file.close()
 	else:
 		print "No BTC to sell."
