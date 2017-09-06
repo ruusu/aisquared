a79bd4746bfa7423a50594bfbc5ee9658312014e
Teemu Alin <teemu.alin@aalto.fi>
2017-09-06 15:37:53 +0300

    cleaned up trades.txt formatting for easier parsing, added approximation of bitfinex fees for testing

--- trademanager.py
+++ trademanager.py
@@ -7,20 +7,27 @@ class portfolio:
 
 btcwallet = portfolio(1.0, 4000.0)
 
+def totalvalue(price):
+	value = price * btcwallet.cryptobalance + btcwallet.eurbalance
+	return price
+
 def buybtc(price):
 	if btcwallet.eurbalance > 0:
-		amount = btcwallet.eurbalance / price
+		amount = (btcwallet.eurbalance * 0.998) / price
 		btcwallet.cryptobalance += amount
-		print "Bought", amount, "BTC for", btcwallet.eurbalance, "EUR."
+		print "Bought", amount, "BTC for", (btcwallet.eurbalance * 0.998), "EUR."
 		amountstr = str(amount)
-		euramountstr = str(btcwallet.eurbalance)
+		euramountstr = str(btcwallet.eurbalance * 0.998)
 		btcwallet.eurbalance = 0
 		print "Current portfolio status:"
 		print "BTC:", btcwallet.cryptobalance
 		print "EUR:", btcwallet.eurbalance
+		value = totalvalue(price)
+		valuestr = str(value)
+		print "Total BTC portfolio value:", value, "eur."
 		file = open("trades.txt", "a")
 		file.write(datetime.datetime.now().ctime())
-		file.write(" BUY %s btc for %s eur.\n" % (amountstr, euramountstr))
+		file.write("|B|%s|%s|%s.\n" % (amountstr, euramountstr, valuestr))
 		file.close() 
 	else:
 		print "Not enough money to make a buy."
@@ -28,17 +35,20 @@ def buybtc(price):
 def sellbtc(price):
 	if btcwallet.cryptobalance > 0:
 		amount = btcwallet.cryptobalance * price
-		btcwallet.eurbalance += amount
-		print "Sold", btcwallet.cryptobalance, "BTC for", amount, "EUR."
-		amountstr = str(amount)
+		btcwallet.eurbalance += (amount * 0.998)
+		print "Sold", btcwallet.cryptobalance, "BTC for", (amount * 0.998), "EUR."
+		amountstr = str(amount * 0.998)
 		btcamountstr = str(btcwallet.cryptobalance)
 		btcwallet.cryptobalance = 0
 		print "Current portfolio status:"
 		print "BTC:", btcwallet.cryptobalance
 		print "EUR:", btcwallet.eurbalance
+		value = totalvalue(price)
+		valuestr = str(value)
+		print "Total BTC portfolio value:", value, "eur."
 		file = open("trades.txt", "a")
 		file.write(datetime.datetime.now().ctime())
-		file.write(" SELL %s btc for %s eur.\n" % (btcamountstr, amountstr))
+		file.write("|S|%s|%s|%s\n" % (btcamountstr, amountstr, valuestr))
 		file.close()
 	else:
 		print "No BTC to sell."
